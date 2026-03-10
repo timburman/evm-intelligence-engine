39cce740ffa4ed7e5088ce1c48f7ac2c38b8c5e6
aryan <aryankaushik251@gmail.com>
2026-03-10 22:34:49 +0530

    Historical Pricing stat

--- backend/core/historical_pricer.py
+++ backend/core/historical_pricer.py
@@ -10,11 +10,13 @@ CG_HISTORY_URL = "https://api.coingecko.com/api/v3/coins/{id}/history"
 LLAMA_HISTORY_URL = "https://coins.llama.fi/prices/historical/{timestamp}/{prefix}:{id}"
 HISTORY_CACHE_FILE = "data/prices/historical_cache.json"
 
+
 class HistoricalPricer:
     """
     The Time Macine lol.
     Waterfall Architecture: Local Cache -> CoinGecko -> DefiLlama
     """
+
     def __init__(self) -> None:
         # Structure {"ethereum": {"2024-05-28": 1850:55}}
         self.historical_cache = {}
@@ -39,10 +41,49 @@ class HistoricalPricer:
         Takes '2023-05-15 14:30:00' and creates the formats needed for APIs.
         Returns: (cache_key, cg_date, unix_timestamp)
         """
-        dt = datetime.strptime(sql_timestamp, '%Y-%m-%d %H:%M%S').replace(tzinfo=timezone.utc)
+        dt = datetime.strptime(sql_timestamp, "%Y-%m-%d %H:%M%S").replace(
+            tzinfo=timezone.utc
+        )
 
-        cache_key = dt.strftime('%Y-%m-%d')
-        cg_date = dt.strftime('%d-%m-%Y')
+        cache_key = dt.strftime("%Y-%m-%d")
+        cg_date = dt.strftime("%d-%m-%Y")
         unix_ts = int(dt.timestamp())
 
         return cache_key, cg_date, unix_ts
+
+    async def get_historical_price(self, coin_id: str, sql_timestamp: str) -> float:
+        """
+        The MC
+        Tries Cache, then CoinGecko and then DefiLlama
+        """
+        if not coin_id:
+            return 0.0
+
+        cache_key, cg_date, unix_ts = self._parse_dates(sql_timestamp)
+
+        # Step 1: Check Cache(0ms, 0Cost)
+        if (
+            coin_id in self.historical_cache
+            and cache_key in self.historical_cache[coin_id]
+        ):
+            return self.historical_cache[coin_id][cache_key]
+
+        print(f"[PRICER] Fetching {coin_id} for {cache_key}...")
+
+        # Step 2: Try CoinGecko
+        price = await self._fetch_coingecko(coin_id, cg_date)
+
+        if price > 0:
+            self._update_cache(coin_id, cache_key, price)
+            return price
+
+        # Step 3: CoinGecko Failed.. Fallback to DefiLlama
+        print(f"[WARN] CoinGecko failed for {coin_id}. Moving to DefiLlama")
+        price = await self._fetch_defillama(coin_id, unix_ts)
+
+        if price > 0:
+            self._update_cache(coin_id, cache_key, price)
+            return price
+
+        print(f"[ERROR] Both APIs failed to find {coin_id} on {cache_key}.")
+        return 0.0
